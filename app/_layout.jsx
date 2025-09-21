d196602d4d73361017cfb37e357f0b1189ed9843
Jose Sainz <jose.sainz@clickbalance.com>
2025-09-20 18:10:40 -0700

    feat: Light and Dark Themes with useColorScheem hook

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Slot, Stack, StatusBar} from 'expo-router'
 import React from 'react'
+import { Colors } from '../constants/Colors'
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-    <Stack screenOptions={{
-      headerStyle: { backgroundColor: '#ddd'},
-      headerTintColor: '#333'
-    }}>
-      <Stack.Screen name="index" options={{ title: 'Home' }}/>
-      <Stack.Screen name="about" options={{ title: 'About' }}/>
-      <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }}/>
-    </Stack>
+    <>
+      <StatusBar value="auto" />
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title,
+      }}>
+        <Stack.Screen name="index" options={{ title: 'Home' }}/>
+        <Stack.Screen name="about" options={{ title: 'About' }}/>
+        <Stack.Screen name="contact" options={{ title: 'Contact'}}/>
+      </Stack>
+    </>
   )
 }
 
--- app/contact.jsx
+++ app/contact.jsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useColorScheme } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { Colors } from '../constants/Colors'
 
 
 const Contact = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: theme.background}]}>
       <Text style={styles.title}>Contact Page</Text>
 
       <Link href="/" style={styles.link}>Back Home</Link>
